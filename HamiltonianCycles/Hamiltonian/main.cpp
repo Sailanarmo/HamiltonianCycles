3ce2e7452b6907fb31b20384061b602eec693b5b
Sailanarmo <rlramirez12@gmail.com>
2017-05-26 21:10:19 -0600

    Adding main file with TODO in them.

---
+++ HamiltonianCycles/Hamiltonian/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+//TODO: Read in a file of Chordal Adjacency Matrices from Dr. Brendan McKay's nauty files, brute force those matrices for Hamiltonian cycles. Output those files into .g6 files to be viewed in graph visualization software. 
+
+//TODO: Future plans will be to try and compute these algorithms on a GPU instead of using one of our CPU's.
+
+int main()
+{
+
+	//do the things
+
+}
