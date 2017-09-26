de98aadc31a8af6f046abfeb4fadea69f4287358
sailanarmo <rlramirez12@gmail.com>
2017-09-25 21:01:27 -0600

    split files into n partitions

---
+++ split.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+if (( $# != 3)); then
+	echo "requires 3 arguments"
+	echo "<file> <number of splits> <file prefix"
+else
+	split -n $2 -d --additional-suffix=.g6 $1 $3
+fi
+
+exit
