3ce1370f0c7f9d179462eef6b0656fb417696656
sailanarmo <rlramirez12@gmail.com>
2017-10-09 15:52:24 -0600

    fixed spelling

--- split.sh
+++ split.sh
@@ -2,7 +2,7 @@
 
 if (( $# != 3)); then
 	echo "requires 3 arguments"
-	echo "<file> <number of splits> <file prefix"
+	echo "<file> <number of splits> <file prefix>"
 else
 	split -n $2 -d --additional-suffix=.g6 $1 $3
 fi
