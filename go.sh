a8dc670b394737b238e75869b50a66e823a6e36e
sailanarmo <rlramirez12@gmail.com>
2017-11-06 13:15:14 -0700

    general script to start ham.out

--- go.sh
+++ go.sh
@@ -1,7 +1,18 @@
 #!/usr/bin/env bash
 
+if [ "$#" -ne 2 ]; then
+  echo "go.sh <vertices> <file prefix>"
+  exit
+fi
+
+cd ~/Documents/HamiltonianCycles/HamiltonianCycles/Hamiltonian
+sed -i "s/\(#define\sV\s\)[0-9]\+/\1$1/" ham.cpp
+make
+cd -
+
+
 for i in {0..8}; do
-  echo starting $i
-  .././listg -a ../../../../Graphs/FilteredWithoutHamiltonian/split12/filteredHamiltonainOn120$i.g6 | ../../../HamiltonianCycles/Hamiltonian/./a.out out0$i.adj &
+  echo "starting $2$i.g6"
+  listg -a $2$i.g6 | ham out0$i.adj &
   sleep .25
 done
